b148c75d0f00b330eae62437e6534d622f50c9e2
Magnus Byrkjeland <m.byrkjeland@student.uq.edu.au>
2024-09-20 16:11:40 +1000

    feat: add page load console log script

---
+++ assessment/ts/page_load_message.ts
@@ -0,0 +1,7 @@
+export const PageLoadMessage = () => {
+  if (typeof window !== 'undefined') {
+    window.addEventListener('load', () => {
+      console.log('Page has loaded javascript');
+    });
+  }
+};
