129530d3e7e12bec6688b795e86559b734997537
Magnus Byrkjeland <m.byrkjeland@student.uq.edu.au>
2024-10-25 10:34:17 +1000

    feat: star icon
    Adding star icon to community card.
    Also adding icon alignment to destination & community cards

--- assessment/js/components/community_post_card.js
+++ assessment/js/components/community_post_card.js
@@ -8,10 +8,10 @@ export default function VerticalCard(post) {
     <img src="${post.image}" alt="Image of ${post.name}" loading="lazy" />
     <div class="card-content">
     <div>
-      <p>By ${post.owner} · <span class="sr-only">Rating:</span>${post.rating}</p>
+      <p class="flex-row-align-center gap-025">By ${post.owner} · <span class="wrapper-rating single"><span class="sr-only">Rating:</span><span aria-hidden="true" class="wrapper-svg svg-star"></span>${post.rating} <span class="sr-only">stars</span></span></p>
     </div>
       <h3 id="post-${post.id}-heading">${post.name}</h3>
-      <p><span class="wrapper-svg svg-map-pin" aria-hidden="true" style="width:0.9rem;height:0.9rem;"></span> ${post.country}</p>
+      <p class="flex-row-align-center gap-025"><span class="wrapper-svg svg-map-pin" aria-hidden="true" style="width:0.9rem;height:0.9rem;"></span> ${post.country}</p>
       <div class="actions mt-auto">
       <a href="./community-post.html?id=${post.id}" class="button translate" aria-label="Explore ${post.name} post">Explore gem<span class="wrapper-svg svg-chevron-right"></span></a>
       </div>
--- assessment/js/components/destination_card.js
+++ assessment/js/components/destination_card.js
@@ -8,7 +8,7 @@ export default function VerticalCard(destination) {
     <img src="${destination.image}" alt="Image of ${destination.name}" loading="lazy" />
     <div class="card-content">
       <h3 id="destination-${destination.id}-heading">${destination.name}</h3>
-      <p><span class="wrapper-svg svg-map-pin" aria-hidden="true" style="width:0.9rem;height:0.9rem;"></span> ${destination.country}</p>
+      <p class="flex-row-align-center gap-025"><span class="wrapper-svg svg-map-pin" aria-hidden="true" style="width:0.9rem;height:0.9rem;"></span> ${destination.country}</p>
       <div class="actions mt-auto">
         <a href="./destination.html?id=${destination.id}" class="button translate" aria-label="Explore ${destination.name} destination">Explore destination<span class="wrapper-svg svg-chevron-right"></span></a>
       </div>
